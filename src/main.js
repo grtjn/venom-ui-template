51501e3d06e33c4169c162cfe13c4cf876e174d8
Geert <grtjn@users.noreply.github.com>
2020-11-29 22:07:13 +0100

    #2/#14: enabled bootstrap, bootstrap-vue, and bootstrap-icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import './styles/custom.scss';
 
 Vue.config.productionTip = false;
 
